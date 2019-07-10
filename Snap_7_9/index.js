7216b850efac0b53f687d8abbb7cf941f1dd886e
Chrystal Copeland <canderson73@cnm.edu>
2019-07-10 09:19:21 -0600

    Snap Challenge part 1 and 2

--- Snap_7_9/index.js
+++ Snap_7_9/index.js
@@ -4,3 +4,32 @@ Take two numbers as arguments and return a new value after a mathematical operat
 
 	Function #2
 Take two strings as arguments and return a value that has them concatenated.*/
+//Function #1
+
+function operation (num1, num2) {
+	return num1 / num2;
+};
+
+
+let opera = operation(3,6)
+console.log(opera)
+
+console.log(operation(3,6))
+
+console.log(operation(7,3))
+console.log(operation(3,7))
+console.log(operation(750,3))
+
+let punk = operation(7,3);
+let jazz = operation(3,7);
+let ska = operation(750,3);
+
+console.log(punk, ska, jazz);
+
+//Function #2
+
+function concatenateTwoStrings (string1, string2) {
+	return string1 + " " string2;
+
+	console.log(contcatenateTwoStrings(string1:'thingOne', string2 'thingTwo'))
+}
