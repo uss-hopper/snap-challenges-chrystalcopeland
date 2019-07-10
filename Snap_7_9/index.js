6b3f4f129a8aec49dfef36e66b39394e4bec2f67
Chrystal Copeland <canderson73@cnm.edu>
2019-07-10 08:32:10 -0600

    Snap Challenge

---
+++ Snap_7_9/index.js
@@ -0,0 +1,6 @@
+/*Write two functions in javascript. Make sure that you put them in your GitHub Classroom account in the Snap Challenges project.  Put them in a folder called "snap_7_9"
+Function #1
+Take two numbers as arguments and return a new value after a mathematical operation (addition, subtraction, multiplication, etc.)  Which operation is your choice.
+
+	Function #2
+Take two strings as arguments and return a value that has them concatenated.*/
