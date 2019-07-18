11b42f3bf0b15363b4739f0607ec85f9778d9dce
Chrystal Copeland <canderson73@cnm.edu>
2019-07-18 08:50:03 -0600

    snap challenge 7_18

---
+++ snap_7_18/functions.js
@@ -0,0 +1,15 @@
+// write algorithm that converts weights into grams
+// convert kilograms to grams
+// 1 gram = .001 kilogram
+
+Given weightKilogram:
+	Set intermediaryVariable to weightKilogram - .001;
+Set weightGram to intermediaryVariable * .001 * 100;
+return weightGram;
+
+//Write a function in JavaScript that takes a number and finds the sum of all
+// unique positive factors of a number.
+// (The factors of a number are all numbers that divide evenly into it.
+// For example, the unique positive factors of 12 are 1, 2, 3, 4, 6, and 12,
+// and the sum would thus be 28.)
+
